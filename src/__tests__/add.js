cffda7eaba9ee1826b9bc5bd9c8613b6998b3329
Maxi Ferreira <charca@gmail.com>
2018-06-14 14:08:07 -0300

    Setup with rollup, babel and jest

---
+++ src/__tests__/add.js
@@ -0,0 +1,7 @@
+import { add } from '../'
+
+describe('Adds two numbers', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3)
+  })
+})
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export const add = (a, b) => {
+  return a + b
+}
+
+export default add
